992bff8999e899e37e9204416e3355e057748adf
wjp <wjp_github@163.com>
2026-06-02 20:09:14 +0800

    feat(pages): 新增用户管理页面

---
+++ src/typings/system/user.d.ts
@@ -0,0 +1,15 @@
+declare namespace User {
+  interface Item {
+    id: number;
+    username: string;
+    password?: string;
+    nickname: string;
+    phone: string;
+    avatar: string;
+    email: string;
+    gender: 0 | 1 | 2;
+    roleIds: number[];
+    status: boolean;
+    remark: string;
+  }
+}
