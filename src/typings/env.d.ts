f924508476cd2fb5842c42ef3ac0f97d36122311
wjp <wjp_github@163.com>
2026-07-15 22:22:10 +0800

    refactor(types): [env] 清理冗余及未被引用的环境变量类型声明

--- src/typings/env.d.ts
+++ src/typings/env.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   readonly MODE: 'dev' | 'test' | 'prod';
-  readonly BASE_URL: string;
   readonly VITE_PUBLIC_PATH: string;
   readonly VITE_APP_PREFIX: string;
   readonly VITE_APP_NAME: string;
@@ -10,9 +9,6 @@ interface ImportMetaEnv {
   readonly VITE_ENTERPRISE_ID: string;
   readonly VITE_AGENT_ID: string;
   readonly VITE_BASE_URL: string;
-  readonly VITE_EXTERNAL_CONTACT_ID: string;
-  readonly VITE_FINANCE_ID: string;
-  readonly VITE_COMMON_URL: string;
 }
 
 interface ImportMeta {
